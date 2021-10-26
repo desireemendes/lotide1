b285457194db7ce8776e358c86d6f392413d0d8e
Desiree Mendes <desireefrost@live.ca>
2021-10-26 14:21:25 -0400

    takes in two arrays, returns true or false based on perfect match

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+//takes in 2 arrays, returns true or false based on perfect match
+
+const eqArrays = function (actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (Array.isArray(actual[i]) && Array.isArray(expected[i])) {
+        if (!eqArrays(actual[i], expected[i])) {
+          return false;
+        }
+      } else if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]))
