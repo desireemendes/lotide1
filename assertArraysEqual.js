bbd3dd2529f6e143284b91288d0d67d41545c15e
Desiree Mendes <desireefrost@live.ca>
2021-10-26 15:10:31 -0400

    function takes in two arrays and displays appropiate message

---
+++ assertArraysEqual.js
@@ -0,0 +1,34 @@
+const eqArrays = function (actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (Array.isArray(actual[i]) && Array.isArray(expected[i])) {
+        if (!eqArrays(actual[i], expected[i])) {
+          return false;
+        }
+      } else if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+
+const assertArraysEqual = (arr1, arr2) => {
+  let results = arr1.length === arr2.length;
+  if (results === false) {
+    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      results === true ? (results = arr1[i] === arr2[i]) : (results = false);
+    }
+  }
+  results
+    ? console.log(`🥳🥳🥳 Assertion Passed: [${arr1}] === [${arr2}]`)
+    : console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  return results;
+};
+
