502cc98b8c1d7309d99ab2397ed564dd1de6b6ca
Desiree Mendes <desireefrost@live.ca>
2021-10-27 19:27:51 -0400

    countLetters implemented

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const result = {};
+  const newString = string.split(" ").join("");
+  for (const letter of newString) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+}
+
+
+
